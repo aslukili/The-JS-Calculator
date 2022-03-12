f1ce5c67471e5da359d5a057f901d93e98fd15d7
Abdeslam Loukili <abdeslam.edu@gmail.com>
2022-03-12 17:13:22 +0000

    nothing yet

---
+++ index.js
@@ -0,0 +1,10 @@
+let resultNumber = [];
+let operator = '';
+let secondNumber = [];
+
+
+let screen = document.getElementById("screen");
+let button = document.querySelectorAll(".button");
+
+console.log(screen);
+console.log(button);
